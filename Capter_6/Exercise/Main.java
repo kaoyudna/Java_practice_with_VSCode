0119c126b35c217ec43c21df31d4bd8c707ab453
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-17 23:55:41 +0900

    add

---
+++ Capter_6/Exercise/Main.java
@@ -0,0 +1,9 @@
+import comment.Zenhan.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        comment.Zenhan.doWarusa();
+        comment.Zenhan.doTogame();
+        comment.Kouhan.callDeae();
+        comment.Kouhan.showMondokoro();
+    }
+}
