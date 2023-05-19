43cfb685d7fded6d7006636dd6cd8e6058f7af7a
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-19 11:52:51 +0900

    addi

--- Capter_8/Hero.java
+++ Capter_8/Hero.java
@@ -1,26 +1,18 @@
 public class Hero {
+
     String name;
     int hp;
-    public void attack() {
+    Sword sword;
 
+    public void attack() {
+        System.out.println(this.name + "は" + sword.name + "で攻撃した!");
+        System.out.println("敵に" + sword.damage + "ダメージを与えた！");
     }
-    public void sleep() {
+    public Hero(String name) {
         this.hp = 100;
-        System.out.println(this.name + "は、眠って回復した！");
+        this.name = name;
     }
-    public void sit(int sec) {
-        this.hp += sec;
-        System.out.println(this.name + "は、" + sec + "秒座った！");
-        System.out.println("HPが" + sec + "ポイント回復した！");
-    } 
-    public void slip() {
-        this.hp -= 5;
-        System.out.println(this.name + "は、転んだ！");
-        System.out.println("5のダメージ!");
-    }
-    public void run() {
-        System.out.println(this.name + "は、逃げ出した！");
-        System.out.println("GAMEOVER");
-        System.out.println("最終HPは" + this.hp + "でした");
+    public Hero() {
+        this("ダミー");
     }
 }
--- Capter_8/Main.java
+++ Capter_8/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Hero h;
-        h = new Hero();
-        h.hp = 100;
+        Hero h1 = new Hero("ミナト");
+        Hero h2 = new Hero("アサカ");
+        Hero h3 = new Hero();
+        Wizard w = new Wizard();
+        w.name = "スガワラ";
+        w.hp = 50;
+        System.out.println("勇者" + h1.name + "、HPは" + h1.hp);
+        System.out.println("勇者" + h2.name + "、HPは" + h2.hp);
+        System.out.println("勇者" + h3.name + "、HPは" + h3.hp);
+        w.heal(h1);
+        w.heal(h2);
+        w.heal(h2);
     }
 }
