03ca0708943944151f2fe469027825eb344b70ad
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-28 16:14:03 +0900

    add

---
+++ Capter_10/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        SuperHero sh = new SuperHero();
+        h.run();
+        sh.run();
+    }
+}
