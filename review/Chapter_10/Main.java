3c20ede7ab4ebc70736cc3d805044117ddc45f11
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-05-29 13:51:29 +0900

    add

---
+++ review/Chapter_10/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        SuperHero sh = new SuperHero();
+        Hero h = new Hero();
+        Matango m = new Matango('A');
+        h.run();
+        sh.run();
+    }
+}
---
+++ review/Chapter_10/Matango.java
@@ -0,0 +1,10 @@
+public class Matango {
+    int hp;
+    int level;
+    char suffix;
+    public Matango(char suffix) {
+        this.hp = 10;
+        this.level = 10;
+        this.suffix = suffix;
+    }
+}
