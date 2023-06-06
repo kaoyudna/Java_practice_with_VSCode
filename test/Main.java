69a94a9f23edfc3a3053bda6881f448d352e98ae
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-06-06 22:34:36 +0900

    add

--- test/Main.java
+++ test/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Hero h = new Hero("太郎");
         Matango m = new Matango('A');
+        Wizard w = new Wizard();
         h.attack(m);
         m.run();
+        w.fireBall(m);
         
     }
 }
